21914b42575e16e286f1da48571908d9bab7e8c3
aidenraadh <aidenraadh@gmail.com>
2022-09-01 20:12:10 +0700

    get subscribe

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -14,11 +14,12 @@ export default async function Subscribe(req, res){
     const options = {
         method: 'POST',
         headers: {
-            "Accept": "application/json",
-            "Content-type": "application/json",
+            Accept: 'application/json',
+            'X-MailerLite-ApiDocs': 'true',
+            'Content-Type': 'application/json',
             'X-MailerLite-ApiKey': '8c569c7547905031e5272208f8e35262'
         },
-        body: JSON.stringify({email: body.email})
+        body: JSON.stringify({email: body.email, resubscribe: false})
     };    
     const response = await fetch('https://api.mailerlite.com/api/v2/subscribers', options);
     
